de9e625de167409d4b45fc9cfd0d9dae62656314
tsnakai <48578242+tsnakai@users.noreply.github.com>
2019-04-15 22:13:24 +0000

    test files

---
+++ test.py
@@ -0,0 +1,12 @@
+#import RPi.GPIO as GPIO
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(23, GPIO.OUT)
+#GPIO.output(23, GPIO.HIGH)
+
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.OUT)
+GPIO.output(23, GPIO.HIGH)
+sleep(1)
+GPIO.cleanup()
